2b49d6370745db44555211da110db945a9f4271b
SAV-web <sigfridoalberto@gmail.com>
2025-11-12 18:18:53 +0100

    modified:   videoclub-vite/.gitignore 	new file:   videoclub-vite/dist/assets/index-BOhbb2pJ.js 	new file:   videoclub-vite/dist/assets/index-BUiwR1f3.css 	new file:   videoclub-vite/dist/assets/manifest-CuK5qHq4.webmanifest 	new file:   videoclub-vite/dist/assets/sprite-tsJ9JXzF.svg 	new file:   videoclub-vite/dist/index.html 	modified:   videoclub-vite/vite.config.js

--- videoclub-vite/vite.config.js
+++ videoclub-vite/vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-base: '/videoclub.digital/',
+base: '/videoclub.digital/videoclub-vite/',
   server: {
     hmr: {
       host: 'localhost',
