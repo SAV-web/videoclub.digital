e98431a922b9ccca5dc892fa48efc578dd5a40d4
SAV-web <sigfridoalberto@gmail.com>
2025-11-13 08:58:07 +0100

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-base: '/videoclub.digital/',
+  base: '/videoclub.digital/', // O la base que estés usando
+  plugins: [
+    createHtmlPlugin({
+      minify: {
+        // Le decimos al plugin de HTML que elimine los comentarios
+        removeComments: true,
+        // (Opcional pero recomendado) También elimina espacios en blanco
+        collapseWhitespace: true, 
+      },
+    }),
+  ],
 });
