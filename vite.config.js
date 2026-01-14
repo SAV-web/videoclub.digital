0ec4ebde940e1159f427cf0c7b08570471d5e2ed
SAV-web <sigfridoalberto@gmail.com>
2026-01-14 20:37:35 +0100

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  base: '/videoclub.digital/', // O la base que estés usando
-  plugins: [
-    createHtmlPlugin({
-      minify: {
-        // Le decimos al plugin de HTML que elimine los comentarios
-        removeComments: true,
-        // (Opcional pero recomendado) También elimina espacios en blanco
-        collapseWhitespace: true, 
-      },
-    }),
-  ],
+  // Base relativa para que los assets carguen correctamente en subdirectorios (GitHub Pages)
+  base: './',
 });
